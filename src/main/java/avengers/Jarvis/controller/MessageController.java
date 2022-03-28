eec70c2580909022799154b126c91dd249381865
ZagainovNA <n.zagainov@id-mt.ru>
2022-03-28 20:48:11 +0300

    add websockets

--- src/main/java/avengers/Jarvis/controller/MessageController.java
+++ src/main/java/avengers/Jarvis/controller/MessageController.java
@@ -6,6 +6,8 @@ import avengers.Jarvis.repository.MessageRepository;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -49,4 +51,12 @@ public class MessageController {
     public void delete(@PathVariable("id") Message message) {
         messageRepository.delete(message);
     }
+
+
+    @MessageMapping("/changeMessage")
+    @SendTo("/topic/activity")
+    public Message change (Message message) {
+        return messageRepository.save(message);
+    }
+
 }
